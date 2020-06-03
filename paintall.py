4340d8fb58b51bbf2a644423fd9c4d42f6d0d08e
Dostoyewski <dostoyewski@yandex.ru>
2020-06-03 23:45:30 +0300

    Added dynamically changed grid and time_horizon param

--- paintall.py
+++ paintall.py
@@ -10,8 +10,6 @@ from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QPushButton, QDo
 
 from konverter import coords_global
 
-TIME_HORIZON = 2
-
 hmi_data = {
     "wind_direction": 189.0,
     "wind_speed": 1.1,
@@ -53,6 +51,7 @@ constraints = {
 
 
 class Widget(QWidget):
+    rs_signal = QtCore.pyqtSignal(QtCore.QSize)
 
     def __init__(self):
         super(Widget, self).__init__()
@@ -61,6 +60,8 @@ class Widget(QWidget):
         self.spinBox1 = QDoubleSpinBox(self)
         # Longitude selection
         self.spinBox2 = QDoubleSpinBox(self)
+        # Time horizont selection
+        self.spinBox3 = QDoubleSpinBox(self)
         self.keepDraw = False
         self.start = QPoint()
         self.end = QPoint()
@@ -72,12 +73,20 @@ class Widget(QWidget):
         self.index = []
         # Scale: pixels in nm
         self.scale = 10
+        # Preferred number of lines in grid
+        self.n_line_x = 10
+        self.n_line_y = 10
+        # Time horizon
+        self.time_horizon = 2
         self.initUI()
 
     def initUI(self):
         imageSize = QtCore.QSize(1366, 768)
         self.image = QtGui.QImage(imageSize, QtGui.QImage.Format_RGB32)
         self.image.fill(QtGui.qRgb(255, 255, 255))
+
+        # Resize signal
+        self.rs_signal.connect(self.resize_grid)
 
         # Clear button
         button = QPushButton('Clear field', self)
@@ -91,23 +100,90 @@ class Widget(QWidget):
         button1.resize(140, 50)
         button1.clicked.connect(self.convert_file)
 
+        # Latitude spinbox
+        # lbe = QLabel(self)
+        # lbe.setText('Start Latitude')
+        # lbe.move(280, 0)
         self.spinBox1.setRange(0, 360)
         self.spinBox1.move(280, 0)
         self.spinBox1.setValue(60)
         self.spinBox1.setSingleStep(0.01)
 
+        # Longitude spinbox
         self.spinBox2.setRange(0, 360)
         self.spinBox2.move(280, 25)
         self.spinBox2.setValue(30)
         self.spinBox2.setSingleStep(0.01)
+
+        # Time horizont
+        self.spinBox3.setRange(0, 10)
+        self.spinBox3.move(360, 15)
+        self.spinBox3.setValue(2)
+        self.spinBox3.setSingleStep(0.1)
+        self.spinBox3.valueChanged.connect(self.update_values)
+
+        self.draw_grid()
+        
+    def update_values(self):
+        """
+        Updating drawing params
+        :return:
+        """
+        self.time_horizon = self.spinBox3.value()
+        print(self.time_horizon)
+
+    def resizeEvent(self, event):
+        """
+        event onResize
+        :param event:
+        :return:
+        """
+        self.rs_signal.emit(self.size())
+
+    def resize_grid(self):
+        """
+        re-draws grid for new size
+        :return:
+        """
+        self.clear_window()
+        self.draw_grid()
 
     def clear_window(self):
         self.image.fill(QtGui.qRgb(255, 255, 255))
         self.type = 'our'
         self.index = []
         self.update()
+        self.draw_grid()
+
+    def draw_grid(self):
+        """
+        This function draws nm grid
+        :return:
+        """
+        painter = QPainter(self.image)
+        # Optimal step — 60 pixels
+        optimal_step = 60
+        self.n_line_x = round(self.width() / optimal_step)
+        self.n_line_y = round(self.height() / optimal_step)
+        stepw = self.width() / self.n_line_x
+        steph = self.height() / self.n_line_y
+        pen = QPen(Qt.black, 1, Qt.SolidLine)
+        pen.setStyle(Qt.DashDotDotLine)
+        painter.setPen(pen)
+        for i in range(self.n_line_x+1):
+            painter.drawLine(i*stepw, 0, i*stepw, self.height())
+            painter.drawText(i*stepw, self.n_line_y*steph - 10,
+                             str(round(i*stepw / self.scale, 2)))
+        for i in range(self.n_line_y + 1):
+            painter.drawLine(0, i * steph, self.width(), i * steph)
+            painter.drawText(self.n_line_x * stepw - 40, self.height() - i * steph,
+                             str(round(i * steph / self.scale, 2)))
 
     def convert_file(self):
+        """
+        Makes JSON scenario files
+        :return:
+        """
         try:
             ship = [item for item in self.index if item['type'] == 'our'][0]
             targets = [item for item in self.index if item['type'] != 'our']
@@ -122,7 +198,7 @@ class Widget(QWidget):
                                    self.spinBox2.value())
             dist = ((target['start'][1] - target['end'][1]) ** 2 +
                     (target['start'][0] - target['end'][0]) ** 2) ** 0.5 / self.scale
-            vel = dist / TIME_HORIZON
+            vel = dist / self.time_horizon
             data.append({'id': 'target',
                          'cat': 0,
                          'lat': coords[0],
@@ -142,11 +218,11 @@ class Widget(QWidget):
                                                       ship['start'][0] - ship['end'][0])) + 90
             dist = ((ship['start'][1] - ship['end'][1])**2 +
                     (ship['start'][0] - ship['end'][0])**2)**0.5 / self.scale
-            vel = dist / TIME_HORIZON
+            vel = dist / self.time_horizon
             route_item = {
                 'begin_angle': course,
                 'curve': 0,
-                'duration': TIME_HORIZON*3600,
+                'duration': self.time_horizon*3600,
                 'lat': self.spinBox1.value(),
                 'lon': self.spinBox2.value(),
                 'length': dist,
@@ -186,17 +262,25 @@ class Widget(QWidget):
         print("Closed")
 
     def paintEvent(self, event, type='circle'):
+        """
+        Event handler for painter
+        :param event:
+        :param type:
+        :return:
+        """
         painter = QPainter(self)
         cur_size = QRect(0, 0, self.width(), self.height())
         temp = self.image.copy(cur_size)
         painter.drawImage(event.rect(), temp)
         # painter.drawLine(self.start, self.end)
         if self.type == 'our':
-            painter.setPen(QtCore.Qt.red)
+            pen = QPen(Qt.red, 2, Qt.SolidLine)
+            painter.setPen(pen)
             painter.drawLine(self.start, self.end)
             painter.drawEllipse(self.end, 10, 10)
         elif self.type == 'foreign':
-            painter.setPen(QtCore.Qt.black)
+            pen = QPen(Qt.black, 2, Qt.SolidLine)
+            painter.setPen(pen)
             painter.drawLine(self.start, self.end)
             painter.drawEllipse(self.end, 10, 10)
 
@@ -209,7 +293,8 @@ class Widget(QWidget):
         if event.button() == QtCore.Qt.LeftButton and self.keepDraw:
             painter = QPainter(self.image)
             if self.type == 'our':
-                painter.setPen(QtCore.Qt.red)
+                pen = QPen(Qt.red, 2, Qt.SolidLine)
+                painter.setPen(pen)
                 painter.drawLine(self.start, self.end)
                 painter.drawEllipse(self.end, 10, 10)
                 self.index.append({'type': self.type,
@@ -217,7 +302,8 @@ class Widget(QWidget):
                                    'end': [self.end.x(), self.end.y()]})
                 self.type = 'foreign'
             elif self.type == 'foreign':
-                painter.setPen(QtCore.Qt.black)
+                pen = QPen(Qt.black, 2, Qt.SolidLine)
+                painter.setPen(pen)
                 painter.drawLine(self.start, self.end)
                 painter.drawEllipse(self.end, 10, 10)
                 self.index.append({'type': self.type,

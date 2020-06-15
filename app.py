57911d03c2a5ddff4a154f8a928d4b34b9421a0f
Mark Tartagashev <mangoozt@gmail.com>
2020-06-15 18:43:02 +0300

    Add speed plot

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import math
 import os
 import sys
 
@@ -32,29 +33,29 @@ class ParamBar(QWidget):
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
 
-        # Show coords
-        self.cbCoords = QCheckBox("Show coords", self)
-        self.verticalLayout.addWidget(self.cbCoords)
-
         # Show dist between ships
-        self.cbDist = QCheckBox("Show dist", self)
+        self.cbDist = QCheckBox("Distance", self)
         self.verticalLayout.addWidget(self.cbDist)
 
+        # Show coords
+        self.cbCoords = QCheckBox("Coords", self)
+        self.verticalLayout.addWidget(self.cbCoords)
+
         # Show Global coordinates
-        self.cbGc = QCheckBox("Show coords in WGS", self)
+        self.cbGc = QCheckBox("WGS", self)
         self.verticalLayout.addWidget(self.cbGc)
         self.horizontalLayout.addLayout(self.verticalLayout)
 
         # Safe radius select
-        self.labelRadius = QLabel('Safe radius:', self)
+        self.labelRadius = QLabel('Radius:', self)
         self.horizontalLayout.addWidget(self.labelRadius)
         self.spinBoxRadius = QDoubleSpinBox(self)
         self.spinBoxRadius.setObjectName("doubleSpinBox")
         self.horizontalLayout.addWidget(self.spinBoxRadius)
 
         # Safe radius select
-        self.labelDistance = QLabel('Safe radius:', self)
-        self.horizontalLayout.addWidget(self.labelRadius)
+        self.labelDistance = QLabel('Distance:', self)
+        self.horizontalLayout.addWidget(self.labelDistance)
         self.spinBoxDist = QDoubleSpinBox(self)
         self.spinBoxDist.setObjectName("doubleSpinBox")
         self.horizontalLayout.addWidget(self.spinBoxDist)
@@ -95,7 +96,7 @@ class App(QMainWindow):
         self.filename = ""
         self.relative = True
         self.m = PlotCanvas(self, width=round(12 * self.scale_x), height=round(8 * self.scale_y))
-        self.vel = PlotCanvas(self, width=round(6 * self.scale_x), height=round(7 * self.scale_y))
+        self.vel = VelocityCanvas(self, width=round(6 * self.scale_x), height=round(7 * self.scale_y))
 
         self.toolbar = NavigationToolbar(self.m, self)
         self.toolbar.hide()
@@ -170,15 +171,15 @@ class App(QMainWindow):
         self.params.spinBoxDist.setSingleStep(0.1)
         self.params.spinBoxDist.valueChanged.connect(self.value_changed)
 
-        # Show text checkbox
-        self.params.cbCoords.move(int(1400 * self.scale_x), int(5 * self.scale_y))
-        self.params.cbCoords.toggle()
-        self.params.cbCoords.stateChanged.connect(self.value_changed)
-
         # Show dist checkbox
         self.params.cbDist.move(int(1400 * self.scale_x), int(30 * self.scale_y))
         self.params.cbDist.toggle()
         self.params.cbDist.stateChanged.connect(self.value_changed)
+
+        # Show coords checkbox
+        self.params.cbCoords.move(int(1400 * self.scale_x), int(5 * self.scale_y))
+        self.params.cbCoords.toggle()
+        self.params.cbCoords.stateChanged.connect(self.show_coords_changed)
 
         # Show WGS checkbox
         self.params.cbGc.move(int(1400 * self.scale_x), int(55 * self.scale_y))
@@ -223,8 +224,8 @@ class App(QMainWindow):
         self.btnPan.move(int(0.677 * self.width() + 40), int(0.933 * self.height()))
         self.btnZoom.move(int(0.677 * self.width() + 80), int(0.933 * self.height()))
 
-        self.vel.move(int(0.667 * self.width()), int(0.132 * self.height()))
-        self.vel.resize(int(0.298 * self.width()), 0.794 * self.height())
+        self.vel.move(int(0.67 * self.width())+5, int(0.132 * self.height()))
+        self.vel.resize(int(0.330 * self.width())-5, 0.794 * self.height())
 
     def resizeEvent(self, event):
         """
@@ -247,7 +248,12 @@ class App(QMainWindow):
     def load(self):
         self.load_data(self.filename)
         self.m.plot_paths(self.data, self.frame, self.route_file)
+        self.vel.plot_paths(self.data)
         self.update_time()
+
+    def show_coords_changed(self):
+        self.params.cbGc.setEnabled(self.params.cbCoords.isChecked())
+        self.value_changed()
 
     def value_changed(self):
         """
@@ -329,6 +335,50 @@ class PlotCanvas(FigureCanvas):
         self.ax1.legend()
         self.ax1.set_ylim(self.ax.get_ylim())
         self.ax1.set_xlim(self.ax.get_xlim())
+        local_time = t - path_data[0]['start_time']
+        h, m, s = math.floor(local_time / 3600), local_time % 3600 / 60, local_time % 60
+        self.ax1.set_title('t=({:.0f}): {:.0f} h {:.0f} min {:.0f} sec'.format(t, h, m, s))
+
+
+class VelocityCanvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=50, height=50, dpi=100):
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+
+        FigureCanvas.__init__(self, self.fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QSizePolicy.Expanding,
+                                   QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        self.ax = self.figure.add_subplot(111)
+        # self.ax1 = self.figure.add_subplot(212)
+        # self.fig.tight_layout()
+        # self.ax.set_facecolor((159 / 255, 212 / 255, 251 / 255))
+
+    def plot_paths(self, path_data):
+        """
+        Plots speed
+        :param path_data: Loaded data
+        """
+        self.ax.clear()
+
+        velocities = [item['length'] / item['duration'] * 3600 for item in path_data[0]["items"]]
+        self.ax.step(range(1, len(path_data[0]["items"]) + 1), velocities, where='post')
+        self.ax.set_xlabel('Number of segment')
+        self.ax.set_ylabel('Speed, knt')
+        self.ax.grid()
+        self.draw()
+
+    # def update_positions(self, path_data, t, distance=5, radius=1.5, coords=False, frame=None):
+    #     self.ax1.clear()
+    #     positions = plot.get_positions(path_data, t)
+    #     plot.plot_positions(self.ax1, positions, coords=coords, frame=frame, radius=radius)
+    #     plot.plot_distances(self.ax1, positions, distance)
+    #     self.ax1.legend()
+    #     self.ax1.set_ylim(self.ax.get_ylim())
+    #     self.ax1.set_xlim(self.ax.get_xlim())
 
 
 if __name__ == '__main__':

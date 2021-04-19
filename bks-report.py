34517d236195f011a6bcab68fa6674d7a0f14242
Mark Tartagashev <mangoozt@gmail.com>
2021-04-19 19:53:48 +0300

    Multiprocessing in bks-report

--- bks-report.py
+++ bks-report.py
@@ -8,9 +8,8 @@ import subprocess
 import time
 from datetime import datetime
 from pathlib import Path
-
+from multiprocessing import Pool
 from matplotlib import pyplot as plt
-
 from plot import plot_from_files, Case
 
 
@@ -18,25 +17,31 @@ def fix_returncode(code):
     return ctypes.c_int32(code).value
 
 
-class Report:
-
+class ReportGenerator:
     def __init__(self, executable):
         self.exe = executable
         self.cases = []
         self.work_dir = os.path.abspath(os.getcwd())
         self.tmpdir = os.path.join(self.work_dir, ".bks_report\\")
+        self.rvo = None
+        self.nopic = None
 
     def generate(self, data_directory, glob='*', rvo=None, nopic=False):
+        self.rvo = rvo
+        self.nopic = nopic
         directories_list = []
         for path in Path(data_directory).glob(glob):
             for root, dirs, files in os.walk(path):
                 if "nav-data.json" in files or 'navigation.json' in files:
                     directories_list.append(os.path.join(data_directory, root))
         directories_list.sort()
-        for directory in directories_list:
-            self.run_case(directory, self.exe, rvo, nopic)
 
-    def run_case(self, datadir, usv, rvo=None, nopic=False):
+        with Pool() as p:
+            cases = p.map(self.run_case, directories_list)
+
+        return Report(cases, self.exe, self.work_dir)
+
+    def run_case(self, datadir):
         working_dir = os.path.abspath(os.getcwd())
         os.chdir(datadir)
 
@@ -57,7 +62,7 @@ class Report:
 
         # Print the exit code.
         exec_time = time.time()
-        completedProc = subprocess.run([usv, "--target-settings", case_filenames['target_settings'],
+        command = [self.exe, "--target-settings", case_filenames['target_settings'],
                                         "--targets", case_filenames['targets_data'],
                                         "--settings", case_filenames['settings'],
                                         "--nav-data", case_filenames['nav_data'],
@@ -67,7 +72,9 @@ class Report:
                                         "--maneuver", case_filenames['maneuvers'],
                                         "--analyse", case_filenames['analyse'],
                                         "--predict", case_filenames['targets_maneuvers'],
-                                        ("--rvo" if rvo is True else "--no-rvo" if rvo is False else "")],
+                                        ("--rvo" if self.rvo is True else "--no-rvo" if self.rvo is False else "")]
+
+        completedProc = subprocess.run(command,
                                        stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         exec_time = time.time() - exec_time
 
@@ -75,15 +82,14 @@ class Report:
               .format(datadir, fix_returncode(completedProc.returncode), exec_time))
         image_data = ""
         nav_report = ""
-        if fix_returncode(completedProc.returncode) in (0, 1) and not nopic:
-            if os.path.isfile("maneuver.json"):
-                fig = plot_from_files("maneuver.json")
+        if not self.nopic:
+            fig = plot_from_files(case_filenames['nav_data'])
 
-                f = io.BytesIO()
-                fig.savefig(f, format="png", dpi=300)
-                image_data = '<img width="100%" src="data:image/png;base64,{}">'.format(
-                    base64.b64encode(f.getvalue()).decode())
-                plt.close(fig)
+            f = io.BytesIO()
+            fig.savefig(f, format="png", dpi=300)
+            image_data = '<img width="100%" src="data:image/png;base64,{}">'.format(
+                base64.b64encode(f.getvalue()).decode())
+            plt.close(fig)
         try:
             with open("nav-report.json", "r") as f:
                 nav_report = json.dumps(json.loads(f.read()), indent=4, sort_keys=True)
@@ -91,13 +97,22 @@ class Report:
             pass
 
         os.chdir(working_dir)
-        self.cases.append({"datadir": datadir,
-                           "proc": completedProc,
-                           "image_data": image_data,
-                           "exec_time": exec_time,
-                           "nav_report": nav_report})
+        return {"datadir": datadir,
+                "proc": completedProc,
+                "image_data": image_data,
+                "exec_time": exec_time,
+                "nav_report": nav_report,
+                "command": command}
 
-    def saveHTML(self, filename):
+
+class Report:
+
+    def __init__(self, cases, executable, work_dir):
+        self.cases = cases
+        self.exe = executable
+        self.work_dir = work_dir
+
+    def save_html(self, filename):
         css = """
         h2 {{margin-top: 2em;}}
         .case {
@@ -166,11 +181,7 @@ class Report:
 <h1>Report from {datetime}</h1>""".format(datetime=datetime.now(), styles=css)
 
         for case in self.cases:
-            if case["proc"].returncode in (0, 1):
-                img_tag = case["image_data"]
-            else:
-                img_tag = ""
-
+            img_tag = case["image_data"]
             html += """<div>
 <h2>{casename}</h2>
 <div class="case">
@@ -217,11 +228,11 @@ if __name__ == "__main__":
         use_rvo = False
 
     if args.working_dir is not None:
-        cur_dir = os.path.abspath(parser.working_dir)
+        cur_dir = os.path.abspath(args.working_dir)
     else:
         cur_dir = os.path.abspath(os.getcwd())
-
+    t0 = time.time()
     usv_executable = os.path.join(cur_dir, args.executable)
-    report = Report(usv_executable)
-    report.generate(cur_dir, glob=args.glob, rvo=use_rvo, nopic=args.nopic)
-    report.saveHTML("report.html")
+    report = ReportGenerator(usv_executable)
+    report.generate(cur_dir, glob=args.glob, rvo=use_rvo, nopic=args.nopic).save_html("report.html")
+    print(f'Total time: {time.time() - t0}')

b85286d14b6fbf31a4981700fecca3a091bd006d
Fedor Kondratenko <dostoyewski@yandex.ru>
2021-07-22 16:12:42 +0300

    Added first working server version

---
+++ autotest/diagnosis/models.py
@@ -0,0 +1,14 @@
+import datetime
+
+from django.db import models
+
+
+class TestingRecording(models.Model):
+    date = models.DateTimeField(default=datetime.datetime.now)
+    file = models.FileField(upload_to='media/')
+    # We will storage all plots data in string format using '::' delimiter
+    code0 = models.TextField(blank=True, default='', max_length=2000)
+    code1 = models.TextField(blank=True, default='', max_length=2000)
+    code2 = models.TextField(blank=True, default='', max_length=2000)
+    code4 = models.TextField(blank=True, default='', max_length=2000)
+    code5 = models.TextField(blank=True, default='', max_length=2000)

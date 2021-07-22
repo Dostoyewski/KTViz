9ddda4b54ea505f35ea654647683d5abee1248ca
Fedor Kondratenko <dostoyewski@yandex.ru>
2021-07-22 18:31:50 +0300

    Added working files uploading

--- autotest/diagnosis/urls.py
+++ autotest/diagnosis/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.main_view, name='main'),
+    path('upload/', views.upload_file, name='upload'),
 ]
--- autotest/diagnosis/views.py
+++ autotest/diagnosis/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+import datetime
 
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from .forms import UploadFileForm
 from .models import TestingRecording
 
 
 def main_view(request):
+    """
+    Plots graphs from info aus DB
+    @param request:
+    @return:
+    """
     recordings = TestingRecording.objects.all()
     s_rec = []
     f = lambda arr: [float(a) for a in arr]
@@ -16,3 +26,23 @@ def main_view(request):
                       "code5": f(rec.code5.split(sep='::')),
                       "dists": f(rec.dists.split(sep='::'))})
     return render(request, 'main.html', context={'data': s_rec})
+
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            obj = TestingRecording()
+            obj.date = datetime.datetime.now()
+            obj.file = form.cleaned_data['file']
+            obj.save()
+            return HttpResponseRedirect(reverse('main'))
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
+
+
+def handle_uploaded_file(f):
+    with open('some/file/name.txt', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)

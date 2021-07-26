403ba67be16430a72a7b1356cc2e1c3ce6969402
Fedor Kondratenko <dostoyewski@yandex.ru>
2021-07-26 19:19:44 +0300

    Added Scenario object and API outputs

--- autotest/diagnosis/models.py
+++ autotest/diagnosis/models.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 
+import pandas as pd
 from django.db import models
 
 from .build_graphs import build_percent_diag
@@ -21,8 +22,7 @@ class TestingRecording(models.Model):
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        filename = os.path.split(self.file.path)
-        filename = os.path.splitext(filename[1])[0]
+        filename = os.path.splitext(os.path.split(self.file.path)[1])[0]
         self.date = datetime.datetime.strptime(filename.split(sep='_')[1], "%Y-%m-%d").date()
         codes = build_percent_diag(self.file.path, 12, 4, 0.5, False)
         self.code0 = process_array(codes[0])
@@ -42,3 +42,45 @@ def process_array(arr):
         if i != len(arr) - 1:
             s += "::"
     return s
+
+
+class ScenariosSet(models.Model):
+    n_targets = models.IntegerField(default=1)
+    n_cases = models.IntegerField(default=0)
+    metafile = models.FileField(upload_to='', blank=True)
+
+    def save(self, *args, **kwargs):
+        filename = os.path.splitext(os.path.split(self.metafile.path)[1])[0]
+        df = pd.read_csv(filename)
+        for name in df['datadirs']:
+            Scenario.objects.create(name=os.path.split(name)[1],
+                                    scenariosSet=self)
+        super().save(*args, **kwargs)
+
+
+class Scenario(models.Model):
+    name = models.TextField(blank=True, default='', max_length=500)
+    num_targets = models.IntegerField(default=1)
+    dist1 = models.FloatField(default=0)
+    dist2 = models.FloatField(default=0)
+    vel1 = models.FloatField(default=0)
+    vel2 = models.FloatField(default=0)
+    vel_our = models.FloatField(default=0)
+    course1 = models.FloatField(default=0)
+    course2 = models.FloatField(default=0)
+    scenariosSet = models.ForeignKey(ScenariosSet, on_delete=models.CASCADE, default=1)
+
+    def save(self, *args, **kwargs):
+        try:
+            super().save(*args, **kwargs)
+            names = self.name.split(sep='_')
+            self.dist1 = names[1]
+            self.dist2 = names[2]
+            self.vel1 = names[3]
+            self.vel2 = names[4]
+            self.vel_our = names[5]
+            self.course1 = names[6]
+            self.course2 = names[7]
+        except IndexError:
+            pass
+        super().save(*args, **kwargs)
--- autotest/diagnosis/views.py
+++ autotest/diagnosis/views.py
@@ -43,9 +43,3 @@ def upload_file(request):
     else:
         form = UploadFileForm()
     return render(request, 'upload.html', {'form': form})
-
-
-def handle_uploaded_file(f):
-    with open('some/file/name.txt', 'wb+') as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)

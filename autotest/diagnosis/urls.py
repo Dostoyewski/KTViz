1e96ecc554ba50785a0da2532a26df8d899700c1
Mark Tartagashev <mangoozt@gmail.com>
2021-09-09 19:29:02 +0300

    Remove autotest

--- autotest/diagnosis/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.main_view, name='main'),
-    path('upload/', views.upload_file, name='upload'),
-    path('upload/meta/', views.upload_metafile, name='upload_meta'),
-]
